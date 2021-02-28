f5c4bb221cedbbd683b81dbaf95918dce15620c2
Blaster Intrude <78591926+blasterintrude@users.noreply.github.com>
2021-02-28 08:35:07 +0530

    Add files via upload

---
+++ exercise_1.py
@@ -0,0 +1,5 @@
+print("Twinkle, twinkle, little star, \n\thow i wonder what you are! \n\t\tup above the world so high, \n\t\tlike a diamod in the sky. \ntwinkle, twinkle, little star, \n\thow i wonder what you are ")import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
---
+++ exercise_4.py
@@ -0,0 +1,3 @@
+first = input("user's first name: ")
+last = input("user's last name: ")
+print("hello " + last + " " + first)
